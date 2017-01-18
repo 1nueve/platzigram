633572180984ff6af577e0e8a29f63bde7d1f625
Jossue Galdamez <jossue@MacBook-Pro-de-Jossue.local>
2017-01-18 00:43:46 -0600

    route

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("soy index.js")
+var page = require("page");
+
+var main = document.getElementById("main-container");
+
+page("/", function(){
+  main.innerHTML = "Home";
+})
+
+page("/signup", function(){
+  main.innerHTML = "Signup";
+})
